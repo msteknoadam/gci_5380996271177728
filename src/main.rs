7f3d0c1eca6f5dbf07b11b04d3947961812435d7
msteknoadam <yesilyurt.gok@gmail.com>
2019-12-03 22:34:56 +0300

    Added main logic & Finished project

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,43 @@ fn inputandprocess() {
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
             let trimmed = input.trim();
+            let inputlen = trimmed.len() as u32;
             match trimmed.parse::<u32>() {
                 Ok(input_int) => {
-                    println!("The number you entered: {}", input_int);
+                    // println!("The number you entered: {}", input_int);
+                    // too many type conversions...
+                    let mut nums = Vec::new();
+                    let mut sum = 0;
+                    let mut sumlist = Vec::new();
+                    for input_num in trimmed.chars() {
+                        let input_num_int = input_num.to_digit(10).unwrap();
+                        nums.push(input_num_int);
+                        sum += input_num_int.pow(inputlen);
+                        let text = format!(
+                            "{current_num}^{input_len}",
+                            current_num = input_num_int,
+                            input_len = inputlen
+                        );
+                        sumlist.push(text);
+                    }
+                    // println!("{:?}", sumlist);
+                    if sum == input_int {
+                        println!(
+                            "{} is an Armstrong Number because: {} = {} = {}",
+                            input_int,
+                            sumlist.join(" + "),
+                            sum,
+                            input_int
+                        );
+                    } else {
+                        println!(
+                            "{} is not an Armstrong Number because: {} = {} != {}",
+                            input_int,
+                            sumlist.join(" + "),
+                            sum,
+                            input_int
+                        );
+                    }
                 }
                 Err(..) => {
                     println!("Your input doesn't look like a number. Please enter a number without any commas etc.");
